ca951faad58498d51bb3a32029931d61bac97925
unknown <soling1210@gmail.com>
2021-10-05 11:33:20 +0900

    _document.js 추가

--- pages/About.js
+++ pages/About.js
@@ -2,8 +2,16 @@ import Layout from "../components/Layout";
 
 const About = () => (
   <div>
-    <Layout />
-    <h1>안녕 오늘은 금요일이야</h1>
+    <Layout>
+      <style jsx>
+        {`
+          h1 {
+            color: green;
+          }
+        `}
+      </style>
+      <h1>안녕 오늘은 금요일이야</h1>
+    </Layout>
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 import Layout from "../components/Layout";
 
-const exam = () => (
+const index = () => (
   <div>
     <Layout />
+    <Head>
+      <title>index page</title>
+    </Head>
     <h1>hello</h1>
     <div>
       <Link href="/about">
@@ -13,4 +17,4 @@ const exam = () => (
   </div>
 );
 
-export default exam;
+export default index;
